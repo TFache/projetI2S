5c924afe7a034d4d2277692c3bb3794c23dc8149
Thomas <thom.fache@orange.fr>
2020-10-21 14:54:07 +0200

    Client PHP : méthodes ajout, suppression et consulter

---
+++ GestionPHP/Gestion.php
@@ -0,0 +1,63 @@
+<!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<title>Gestion Etudiants</title>
+<body>
+<?php 
+$action = "";//Nous sert pour l'affichage après une action
+?>
+<h1>Cr&eacute;er un &eacute;tudiant</h1>
+	<form action="Gestion.php" method="POST">
+		Nom : <input type="text" name ="nom"/><br>
+		Prenom : <input type="text" name ="prenom"/><br>
+		Email : <input type="text" name ="email"/><br>
+		<input type = "submit" name = "action" value = "ajouter"/>
+		<?php 
+		if(isset($_POST['action']) && ($_POST['action']=='ajouter')){
+		    $clientSoap = new SoapClient("http://localhost:9594/?wsdl");
+		    if(isset($_POST["nom"]) && isset($_POST["prenom"]) && isset($_POST["email"])) {
+		        $etudiant = array("arg0"=>$_POST["nom"], "arg1"=>$_POST["prenom"], "arg2"=>$_POST["email"]);
+		        $clientSoap->ajouter($etudiant);
+		        $action = "ajouter";
+		
+		    }
+		}
+        ?>
+    </form>
+    <?php if($action == "ajouter") echo "Ajout effectu&eacute; !"?>
+    
+<!-- Supprimer un élève -->    
+<h1>Supprimer un &eacute;tudiant</h1>
+	<form action="Gestion.php" method="POST">
+		ID : <input type ="number" name="id_supp" min="0"/><br>
+		<input type="submit" name="action" value="supprimer"/>
+		<?php 
+		if(isset($_POST['action']) && ($_POST['action']=='supprimer')){
+		    $clientSoap = new SoapClient("http://localhost:9594/?wsdl");
+		    $param = array("arg0"=>$_POST["id_supp"]);
+		    $clientSoap->suppression($param);
+		    $action="supprimer";
+		}
+		?>
+	</form>
+	<?php if($action == "supprimer") echo "Suppression effectu&eacute; !"?>
+	
+<!-- Consulter un élève -->
+<h1>Consulter un &eacute;tudiant</h1>
+	<form action="Gestion.php" method="POST">
+			ID : <input type ="number" name="id_cons" min="0"/><br>
+			<input type="submit" name="action" value="consulter"/>
+			<?php 
+			if(isset($_POST['action']) && ($_POST['action']=='consulter')){
+			    $clientSoap = new SoapClient("http://localhost:9594/?wsdl");
+			    $param = array("arg0"=>$_POST["id_cons"]);
+			    $result = $clientSoap->consulter($param);
+			    $action = "consulter";
+			    
+			}
+			?>
+			
+	</form>
+	<?php if($action == "consulter") echo "Etudiant : ",print_r((array)$result->return); ?>
+</body>
+</html>
